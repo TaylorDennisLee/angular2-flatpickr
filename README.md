98907a62faf0886b1b8257bdbc1662c66cf4a1b3
Alexander Ciesielski <alexander@ciesielski.co>
2016-12-21 10:58:27 +0100

    v0.0.1-alpha

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# CcoFlatpickr
+# Angular 2 Flatpickr
 
 This project was generated with [angular-cli](https://github.com/angular/angular-cli) version 1.0.0-beta.22-1.
 
--- src/app/flatpickr/flatpickr.module.ts
+++ src/app/flatpickr/flatpickr.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { FlatpickrComponent } from './flatpickr.component';
 
+export { Flatpickr, FlatpickrOptions } from './models'
+
 @NgModule({
     imports: [
         CommonModule,
